b02a6f3c6ea7e46f4c4f296c4c2d40b0e3000db8
Khalid MAJDOUB <majdoub.khalid@gmail.com>
2019-03-10 21:39:10 +0100

    Add files via upload

---
+++ part_2/exercise_2_Forwarder/forwarder_server.py
@@ -0,0 +1,10 @@
+import zmq
+import random
+import sys
+import time
+
+# TODO :
+# Create a socket of type PUB
+# Connect it to the localhost via the port 5559 
+# Generate a topic 
+# publishe messages under that topic and wait sometime in between 
---
+++ part_2/exercise_2_Forwarder/forwarder_subscriber.py
@@ -0,0 +1,9 @@
+import sys
+import zmq
+
+
+# TODO :
+# Create a socket of type SUB
+# Connect it to the localhost via the port 5560 
+# Set a topic filter
+# Receive the published messages under that topic and print them
