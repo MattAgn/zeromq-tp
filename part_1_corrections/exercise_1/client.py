2067a30975400a8239e261cceea169f7837d5b77
MattAgn <mat.alingrin@gmail.com>
2019-03-12 15:51:52 +0100

    Updating presentations and putting back in master branch part 2 corrections

--- part_1_corrections/exercise_1/client.py
+++ part_1_corrections/exercise_1/client.py
@@ -9,13 +9,13 @@ import zmq
 context = zmq.Context()
 
 #  Socket to talk to server
-print("Connecting to hello world server…")
+print("Connecting to hello world server")
 socket = context.socket(zmq.REQ)
 socket.connect("tcp://localhost:5555")
 
 #  Do 10 requests, waiting each time for a response
 for request in range(10):
-    print("Sending request %s …" % request)
+    print("Sending request %s" % request)
     socket.send(b"Hello")
 
     #  Get the reply.
